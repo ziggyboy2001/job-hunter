f916f43fe36afb2d6f57bd4b366a0f9cac88fb4a
ziggyboy2001 <brentpurks1@icloud.com>
2023-04-12 21:04:37 -0500

    comitted

---
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+
+import styles from './nearbyjobcard.style'
+
+
+
+const NearbyJobCard = ({job, handleNavigate}) => {
+  return (
+    <TouchableOpacity
+    style={styles.container}
+    onPress={handleNavigate}>
+ <TouchableOpacity style={styles.logoContainer}>
+  <Image 
+  source={{ uri: job.employer_logo}}
+  resizeMode="contain"
+  style={styles.logoImage}
+  />
+ </TouchableOpacity>
+
+ <View style={styles.textContainer}>
+  <Text style={styles.jobName}numberOfLines={1}>{job.job_title}</Text>
+  <Text style={styles.type}>{job.job_employment_type}</Text>
+ </View>
+    </TouchableOpacity>
+  )
+}
+
+export default NearbyJobCard
---
+++ hook/useFetch.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": "10903221a1mshedd4a7be9ba548bp12f649jsn66278f1d8a80",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+  //   axios
+  //     .request(options)
+  //     .then(function (response) {
+  //       console.log(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
